1706470651f2e494b9d678e1b938c5eb9a654dd7
MeckeJadeHS <mecke@jade-hs.de>
2020-04-30 18:33:38 +0200

    Kreisdemo vollständig
    Der Code aus der Vorlesung mit Kommentaren

---
+++ KreisDemo/KreisDemo/Kreis.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace KreisDemo
+{
+    /// <summary>Formal description of a Kreis</summary>
+    class Kreis
+    {
+        private double x;
+        private double y;
+        private double r;
+
+        /// <summary>Constructor of Kreis - 
+        /// this method is called as soon as the "Kreis".
+        /// </summary>
+        public Kreis()
+        {
+            x = 0;
+            y = 0;
+            r = 1;
+        }
+
+        /// <summary>Constructor of Kreis - 
+        /// this method is called as soon as the "Kreis" object gets 
+        /// created with te given parameters.</summary>
+        /// <param name="v">the new x-coordinate.</param>
+        /// <param name="w">the new y-coordinate.</param>
+        public Kreis(double v, double w)
+        {
+            x = v;
+            y = w;
+        }
+
+        /// <summary>This method changes the point's location to
+        ///    the given coordinates.</summary>
+        /// <param name="local_x">the new x-coordinate.</param>
+        /// <param name="local_y">the new y-coordinate.</param>
+        public void setMittelpunkt(double local_x, double local_y)
+        {
+            x = local_x;
+            y = local_y;
+        }
+
+        /// <summary>This method changes the Kreis's radius.</summary>
+        /// <param name="local_r">the new radius.</param>
+        internal void setRadius(double local_r)
+        {
+            r = local_r;
+        }
+
+        /// <summary>This method returns the surface area of a Kreis.</summary>
+        public double getFlaeche()
+        {
+            double res;
+            res = (Math.PI * Math.Pow(r, 2));
+            return res;
+        }
+
+    }
+}
---
+++ KreisDemo/KreisDemo/KreisControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreisDemo
+{
+
+    class KreisControl
+    {
+        KreisControl() // Konstruktor
+        {
+            // Kreise erzeugen
+            /// Objekt Instanziieren
+            Kreis k1;
+            k1 = new Kreis();
+            Kreis k2 = new Kreis(1.0d, 2.0d);
+
+            /// Eigenschaften setzen
+            k1.setMittelpunkt(10.0d, 20);
+            k2.setMittelpunkt(0, 70);
+            k1.setRadius(4.0d);
+            k2.setRadius(1.0d);
+
+            // Fläche berechnen und ausgeben
+            double flaecheK1 = k1.getFlaeche();
+            Console.WriteLine("k1 Flaeche: " + flaecheK1);
+            Console.WriteLine("k2 Flaeche: " + k2.getFlaeche());
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            new KreisControl();
+        }
+    }
+
+}
